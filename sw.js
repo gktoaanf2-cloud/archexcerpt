496fd33b1f2eb755c8f53b1c1be2da563068e7ed
gktoaanf2-cloud <gktoaanf2@gmail.com>
2026-05-20 19:59:02 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,8 @@
-const CACHE_NAME = 'arch-excerpt-v2';
+const CACHE_NAME = 'arch-v5-cache';
 const ASSETS = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); }));
-    }).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
